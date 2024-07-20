8bac5d6024c161daaa2071ed8a6d7661da9d064e
curbinegames <joint.the.air@gmail.com>
2024-07-20 09:20:41 +0900

    fix sancur

--- sancur.cpp
+++ sancur.cpp
@@ -3,51 +3,45 @@
 #include "sancur.h"
 
 /**
- * この関数はもう使っちゃダメ、代わりにmins_2を使ってください
- * 大きいほうを返す
- */
-int mins(int a, int b) {
-	if (a < b)a = b;
-	return a;
-}
-
-/**
  * 小さいほうを返す
  */
 int mins_2(int a, int b) {
-	if (a > b) {
-		return b;
-	}
+	if (a > b) { return b; }
 	return a;
 }
 
 /**
- * この関数はもう使っちゃダメ、代わりにmaxs_2を使ってください
+ * この関数はもう使っちゃダメ、代わりにmins_2を使ってください
  * 小さいほうを返す
  */
 int maxs(int a, int b) {
-	if (a > b)a = b;
-	return a;
+	return mins_2(a, b);
 }
 
 /**
  * 大きいほうを返す
  */
 int maxs_2(int a, int b) {
-	if (a < b) {
-		return b;
-	}
+	if (a < b) { return b; }
 	return a;
+}
+
+/**
+ * この関数はもう使っちゃダメ、代わりにmins_2を使ってください
+ * 大きいほうを返す
+ */
+int mins(int a, int b) {
+	return maxs_2(a, b);
 }
 
 //aをbまで引き上げたものを返す
 double mins_D(double a, double b) {
-	if (a < b)a = b;
+	if (a < b) { a = b; }
 	return a;
 }
 //aをbまで引き下げたものを返す
 double maxs_D(double a, double b) {
-	if (a > b)a = b;
+	if (a > b) { a = b; }
 	return a;
 }
 //bをaまで引き上げ、cまで引き下げたものを返す
@@ -64,18 +58,22 @@ double betweens_D(double a, double b, double c) {
 }
 //aが0だったら1にする
 int notzero(int a) {
-	if (a == 0)a = 1;
+	if (a == 0) { a = 1; }
 	return a;
 }
 //点1(x1,y1)、点2(x2,y2)、を通る直線を考えて、xの時の値を返す。
 int lins(double x1, double y1, double x2, double y2, double x) {
-	if (x1 == x2) return x1;
-	else return (y2 - y1) / (x2 - x1) * (x - x1) + y1;
+	if (x1 == x2) { return x1; }
+	return (y2 - y1) * (x - x1) / (x2 - x1) + y1;
 }
 //頂点(x1,y1)、点(x2,y2)、を通る二次関数を考えて、xの時の値を返す。
 int pals(double x1, double y1, double x2, double y2, double x) {
-	if (x1 == x2) return x1;
-	else return (y2 - y1) / (x2 - x1) / (x2 - x1) * (x - x1) * (x - x1) + y1;
+	double s1 = 0;
+	double s2 = 0;
+	if (x1 == x2) { return x1; }
+	s1 = x - x1;
+	s2 = x2 - x1;
+	return (y2 - y1) * s1 * s1 / (s2 * s2) + y1;
 }
 //mode(1:lin,2:acc,3:dec)に合わせた計算をする。
 double movecal(int mode, double startx, double starty, double endx, double endy, double nowx) {
@@ -94,24 +92,22 @@ double movecal(int mode, double startx, double starty, double endx, double endy,
 double sanrute(double c) {
 	short int d = 10;
 	double a = 0, b = 1;
-	while (b * b < c) b *= 10;
-	if (b * b == c) return b;
+	while (b * b < c) { b *= 10; }
+	if (b * b == c) { return b; }
 	b /= 10;
 	while (b >= 100000 || d == -10) {
-		while (a * a < c) a += b;
-		if (a * a == c) return a;
+		while (a * a < c) { a += b; }
+		if (a * a == c) { return a; }
 		a -= b;
 		b /= d;
-		if (b == 1) d *= -1;
+		if (b == 1) { d *= -1; }
 	}
 	return a;
 }
 //aとbの絶対値を求める
 int abss(int a, int b) {
 	a -= b;
-	if (a < 0) {
-		a *= -1;
-	}
+	if (a < 0) { a *= -1; }
 	return a;
 }
 //aを0からbでループさせる
@@ -140,21 +136,11 @@ double sinC(int a) {
 	0.9659,0.9703,0.9744,0.9781,0.9816,
 	0.9848,0.9877,0.9903,0.9925,0.9945,
 	0.9962,0.9976,0.9986,0.9994,0.9998,1.0000 };
-	while (a < 0) {
-		a += 360;
-	}
-	while (a >= 360) {
-		a -= 360;
-	}
-	if (91 <= a && a <= 179) {
-		return sin[-(a - 180)];
-	}
-	if (180 <= a && a <= 270) {
-		return -sin[a - 180];
-	}
-	if (271 <= a && a <= 359) {
-		return -sin[-(a - 360)];
-	}
+	while (a < 0) { a += 360; }
+	while (a >= 360) { a -= 360; }
+	if (91 <= a && a <= 179) { return sin[-(a - 180)]; }
+	if (180 <= a && a <= 270) { return -sin[a - 180]; }
+	if (271 <= a && a <= 359) { return -sin[-(a - 360)]; }
 	return sin[a];
 }
 //cos"a"を出力する
@@ -167,4 +153,5 @@ void rot_xy_pos(int rot, int *x, int *y) {
 	int G = *x;
 	*x = *x * cosC(rot) - *y * sinC(rot);
 	*y = G * sinC(rot) + *y * cosC(rot);
+	return;
 }
