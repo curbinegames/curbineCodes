c42a014e5442223f62eb295aec169e7aa8103ce6
curbinegames <joint.the.air@gmail.com>
2024-07-25 01:21:01 +0900

    avoid strcur buffer overrun

--- strcur.cpp
+++ strcur.cpp
@@ -29,6 +29,22 @@ int strands(const TCHAR *p1, const TCHAR *p2) {
 }
 
 /**
+ * p1の先頭にp2があるかどうかを調べる
+ * @param[in] p1 探す場所
+ * @param[in] p1size p1の長さ、配列数で指定
+ * @param[in] p2 探す文字列
+ * @param[in] p2size p2の長さ、配列数で指定
+ * @return int あったら1、なかったら0
+ */
+int strands_2(const TCHAR *p1, size_t p1size, const TCHAR *p2, size_t p2size) {
+	for (int i = 0; i < p1size && i < p2size; i++) {
+		if (p2[i] == _T('\0')) { break; }
+		if (p1[i] != p2[i]) { return 0; }
+	}
+	return 1;
+}
+
+/**
  * バッファオーバーランを起こす可能性がある為、この関数はもう使っちゃダメ、代わりにstrcopy_2を使ってください
  * @param[in] p1 コピーするもの
  * @param[in] p2 コピー先
@@ -60,6 +76,28 @@ void strcopy_2(const TCHAR *p1, TCHAR *p2, size_t size) {
  * @details p1から先頭a文字を消す
  */
 void strmods(TCHAR *p1, int a) {
+	int i;
+	TCHAR *p2 = p1;
+	for (i = 0; i < a; i++) {
+		if (p1[i] == _T('\0')) {
+			p1[0] = _T('\0');
+			return;
+		}
+	}
+	for (i = 0; p2[i + a] != _T('\0'); i++) {
+		p1[i] = p2[i + a];
+	}
+	p1[i] = _T('\0');
+	return;
+}
+
+/**
+ * p1から先頭a文字を消す
+ * @param[out] p1 対象の文字列
+ * @param[in] size p1のサイズ
+ * @param[in] a 消す文字数
+ */
+void strmods_2(TCHAR *p1, size_t size, int a) {
 	int i;
 	TCHAR *p2 = p1;
 	for (i = 0; i < a; i++) {
@@ -138,6 +176,22 @@ int strlens(const TCHAR* s) {
 /**
  * 文字列sの長さを取得する
  * @param[in] s 対象となる文字列
+ * @param[in] size sののサイズ、配列数で指定
+ * @return int sの長さ
+ */
+int strlens_2(const TCHAR* s, size_t size) {
+	int ret = 0;
+	for (ret = 0; ret < size; ret++) {
+		if (s[ret] == _T('\0')) {
+			break;
+		}
+	}
+	return ret;
+}
+
+/**
+ * 文字列sの長さを取得する
+ * @param[in] s 対象となる文字列
  * @return int sの長さ
  * @note この関数いる?
  */
@@ -172,6 +226,19 @@ void strnex(TCHAR *p1) {
 }
 
 /**
+ * 次の'/'か':'があるところまで消す
+ * @param[out] p1 対象となる文字列
+ * @param[in] size p1のサイズ、配列数で指定
+ */
+void strnex_2(TCHAR *p1, size_t size) {
+	int i = 1;
+	for (i = 0; i < size && p1[i] >= _T(' ') && p1[i] <= _T('}') && p1[i] != _T('/') && p1[i] != _T(':') && p1[i] != _T('\0'); i++) { ; }
+	if (p1[i] == _T('/') || p1[i] == _T(':')) { i++; }
+	strmods(p1, i);
+	return;
+}
+
+/**
  * バッファオーバーランを起こす可能性がある為、この関数はもう使っちゃダメ、代わりにstrnex_EX2を使ってください
  * @param[out] p1 対象となる文字列
  * @param[in] p3 指定の文字
@@ -179,6 +246,22 @@ void strnex(TCHAR *p1) {
  * @details 指定の文字p3があるところまで消す
  */
 void strnex_EX(TCHAR *p1, TCHAR p3) {
+	TCHAR *p2 = p1;
+	short int a = 1;
+	while (*p1 >= _T(' ') && *p1 <= _T('}') && *p1 != p3 && *p1 != _T('\0')) {
+		a++;
+		p1++;
+	}
+	strmods(p2, a);
+	return;
+}
+
+/**
+ * 指定の文字p3があるところまで消す
+ * @param[out] p1 対象となる文字列
+ * @param[in] p3 指定の文字
+ */
+void strnex_EX2(TCHAR *p1, TCHAR p3) {
 	TCHAR *p2 = p1;
 	short int a = 1;
 	while (*p1 >= _T(' ') && *p1 <= _T('}') && *p1 != p3 && *p1 != _T('\0')) {
@@ -252,12 +335,65 @@ int strsans(const TCHAR *p1) {
 }
 
 /**
+ * 文字列を数値に変換にする。
+ * @param[in] p1 変換する文字列
+ * @param[in] size p1のサイズ、配列数で指定
+ * @return int 変換結果
+ * @su strsansD
+ * @note 少数以下は無視されます。少数以下も変換したい場合はstrsansD()を使用してください
+ * 途中に数字,マイナス以外の文字があったら終わり.
+ */
+int strsans_3(const TCHAR *p1, size_t size) {
+	int a = 0, b = 1;
+	if (*p1 == _T('R')) { return strrans(p1); }
+
+	for (int i = 0; i < 50; i++) {
+		if (_T('0') <= *p1 && *p1 <= _T('9')) { a = a * 10 + (*p1 - _T('0')); }
+		else if (*p1 == _T('-')) { b *= -1; }
+		else { break; }
+		p1++;
+	}
+
+	return a * b;
+}
+
+/**
  * バッファオーバーランを起こす可能性がある為、この関数はもう使っちゃダメ、代わりにstrsansDを使ってください
  * @param[in] p1 変換する文字列
  * @return double 変換結果
  * @details strsans()の小数を可にしたもの
  */
 double strsans2(const TCHAR *p1) {
+	short int b = 1, c = 0, d = 99, i;
+	double a = 0;
+	if (*p1 == _T('R')) { return strrans(p1); }
+
+	for (int ip = 0; ip < 50; ip++) {
+		if (_T('0') <= *p1 && *p1 <= _T('9')) {
+			c++;
+			a = a * 10 + (*p1 - _T('0'));
+		}
+		else if (*p1 == _T('-')) { b *= -1; }
+		else if (*p1 == _T('.')) { d = c; }
+		else {
+			for (i = c; i > d; i--) { a /= 10.0; }
+			break;
+		}
+		p1++;
+	}
+
+	return b * a;
+}
+
+/**
+ * 文字列を数値に変換にする。
+ * @param[in] p1 変換する文字列
+ * @param[in] size p1のサイズ、配列数で指定
+ * @return double 変換結果
+ * @sa strsans_3
+ * @note この関数は少数以下も変換します。整数のみで良い場合はstrsans_3を使用してください。
+ */
+double strsansD(const TCHAR *p1, size_t size) {
 	short int b = 1, c = 0, d = 99, i;
 	double a = 0;
 	if (*p1 == _T('R')) { return strrans(p1); }
